d0ee526610f4f241ae5144767ab620ae69474b49
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-10-14 22:14:13 +0530

    feat: add subsidiaries component and data for displaying subsidiary information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroHome from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
+import Subsidiaries from "@/components/Subsidiaries";
 
 export default function Home() {
   return (
     <>
       <HeroHome />
       <AboutSection />
+      <Subsidiaries />
     </>
   );
 }
